1446f7ac7801cca30ca07aa424fa72230ab4c9c9
Jason Nash <kareem_shahawe@hotmail.com>
2009-11-08 11:58:03 +0200

    my first comment and update..

---
+++ test/Clac.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace testing_proj
+{
+    class Clac
+    {
+        private int number;
+        public Clac(int number)
+        { this.number = number; }
+
+        public int calculate()
+        {
+            this.number += 10;
+            return number;
+        }
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using testing_proj;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clac op = new Clac(20);
+            Console.WriteLine(op.calculate());
+        }
+    }
+}
